331ca71d7a7b210925197e571b9b75b886feeeb3
silvia <silviavarela96@iesamachado.org>
2025-05-18 16:44:01 +0200

    repaso examen 3era ev

--- JavaScript/REPASO-EXAMEN/Ej-Nombre/script.js
+++ JavaScript/REPASO-EXAMEN/Ej-Nombre/script.js
@@ -1,23 +1,38 @@
-document.getElementById('formularioRegistro').addEventListener('submit', function(e) {
+$(document).ready(function () {
 
-    e.preventDefault();
 // Función para mostrar mensaje de bienvenida
 function mensaje() {
-let mensaje = `¡Bienvenido/a, ${nombre}!`;
-alert(mensaje);
+$('#mensajeBienvenida')
+            .text(`¡Bienvenido, ${nombre}!`)
+            .css({
+                "background-color": 'blue',
+                "color": "#00796b",
+                "font-size": "24px",
+                "padding": "20px",
+                "margin-top": "20px",
+                "border-radius": "10px",
+                "text-align": "center"
+            })            
+}
+
+ // obtener el nombre 
+const nombre = document.getElementById('nombre').value;
+    if (nombre === '') {   //si el campo está vacio mensaje de alerta
+        alert('Por favor, introduce tu nombre.');
+    }
+    else {
+        cargarNombre(); //si no está vacio llamamos a la función cargarNombre
+        mensaje();  //si hay nombre nos muestra el mensaje de bienvenida
+    }
     
 localStorage.setItem('nombre', nombre); //para guardar el nombre 
-}
+
 
 function cargarNombre() {
 const nombreGuardado = localStorage.getItem('nombre');
-
     if (nombreGuardado) {
     mensajeBienvenida(nombreGuardado);
     }
 }
 
-
-
 });
-
